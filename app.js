f68d898c6fb25be43a89c52d5687952658c8d6f1
anjali252001 <kumariianjali91@gmail.com>
2021-09-16 20:21:56 +0530

    Level 4 commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true},{useUnifiedTopolgy:true});
 const app=express();
 app.use(express.static("public"));
@@ -14,8 +15,7 @@ const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
-console.log(process.env.API_KEY);
-userSchema.plugin(encrypt,{secret:process.env.SECRET},{encryptedFields:["password"]});
+console.log(md5("2345"));
 const User =new mongoose.model("User",userSchema);
 app.get("/" ,function(req,res){
     res.render("home")
@@ -29,7 +29,7 @@ app.get("/register" ,function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(err)
@@ -41,7 +41,7 @@ app.post("/register",function(req,res){
 });
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     User.findOne({email:username},function(err,foundUser){
         if(err){
             console.log(err);
